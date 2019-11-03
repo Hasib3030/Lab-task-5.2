51a88aaf3d20b0c1ac186b9544fa330b7464f464
Hasib Ahmed <55645743+Hasib3030@users.noreply.github.com>
2019-11-03 11:00:01 +0600

    Add files via upload

---
+++ home.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>User</title>
+</head>
+<body>
+    <form method="POST" action="#">
+	  <fieldset>
+	  <legend>Home</legend> 
+       <table border ="1" width="100%">
+	        
+				<tr>
+				   
+				    <td>
+					    <table>
+						   <td width= "70%">
+						     <h2>X Company </h2>
+							 
+						   </td>
+						   
+						
+						</table>
+					
+					</td>
+					
+					<td width ="30%">
+						    <a href='home.php'>Home</a>
+							  <a href="login.php">Login</a>
+							   <a href="reg.php">Registration</a>
+					</td>
+					
+					
+				    
+				</tr>
+				<tr >
+				<td width ="100%" colspan="2">
+				 <h2>Welcome to XCompany</h2>
+				</td>
+				</tr>
+				<tr>
+				<td width ="100%" colspan="2">
+				<center>
+				 <h>Copyright @ 2017</h>
+				</center>
+				</td>
+				</tr>
+	
+            </table>
+        
+        </center>
+	    </fieldset>
+    </form>
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   
+    <title>User</title>
+</head>
+<body>
+    <form method="POST" action="#">
+	  <fieldset>
+	  <legend>Login</legend> 
+       <table border ="1" width="100%">
+	        
+				<tr>
+				   
+				    <td>
+					    <table>
+						   <td width= "70%">
+						     <h2>X Company </h2>
+							 
+						   </td>
+						   
+						
+						</table>
+					
+					</td>
+					
+					<td width ="30%">
+						    <a href='home.php'>Home</a>
+							  <a href="login.php">Login</a>
+							   <a href="reg.php">Registration</a>
+					</td>
+					
+					
+				    
+				</tr>
+				<tr>
+					<td >Username: </td>
+					<td><input type="text" name=""  /></td>
+					
+				</tr>
+				<tr>
+					<td colspan="2">
+						<hr>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>Password:</td>
+					<td><input type="password" name="" /></td>
+				</tr>
+				<tr>
+				<td width ="100%" colspan="2">
+				<center>
+				 <h>Copyright @ 2017</h>
+				</center>
+				</td>
+				</tr>
+	
+            </table>
+        
+        </center>
+	    </fieldset>
+    </form>
+    
+</body>
+</html>
+
